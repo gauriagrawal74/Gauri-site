adce84139ab9b85884458b893e7577e1aafcd30a
gauriagrawal74 <gauri.agr@gmail.com>
2026-02-24 21:50:58 +0530

    Update eleventy.config.js
    added gauri folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,4 +4,10 @@ import YAML from "yaml";
 export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("public");
 	eleventyConfig.addDataExtension("yaml", (contents) => YAML.parse(contents));
+
+	return {
+    pathPrefix: "/Gauri/"
+  };
 };
+
+
