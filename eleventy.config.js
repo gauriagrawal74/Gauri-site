ad8165dfe33de413f90067700430bace92261452
Gauri Agrawal <gauri.agr@gmail.com>
2026-02-25 11:27:11 +0530

    added gauri-site

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,4 +4,7 @@ import YAML from "yaml";
 export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("public");
 	eleventyConfig.addDataExtension("yaml", (contents) => YAML.parse(contents));
+	 return {
+    pathPrefix: "/Gauri-site/"
+  };
 };
